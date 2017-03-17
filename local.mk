1b433ed1129b9242331bfefa000c3dc9ab356f05
Sander van der Maar <sandervandermaar@gmail.com>
2017-03-17 15:19:26 +0100

    Adds WPE packages

---
+++ local.mk
@@ -0,0 +1,18 @@
+#WEBBRIDGE_OVERRIDE_SRCDIR = /home/sander/Projects/webbridge/eclipse/src
+#CPPSDK_OVERRIDE_SRCDIR = /home/sander/Projects/cppsdk/eclipse/src/cppsdk
+WPEFRAMEWORK_DEVICEINFO_OVERRIDE_SRCDIR = /home/sander/Projects/webbridge/WPEFramework/DeviceInfo
+WPEFRAMEWORK_DIALSERVER_OVERRIDE_SRCDIR = /home/sander/Projects/webbridge/WPEFramework/DIALServer
+WPEFRAMEWORK_MONITOR_OVERRIDE_SRCDIR = /home/sander/Projects/webbridge/WPEFramework/Monitor
+WPEFRAMEWORK_NETFLIX_OVERRIDE_SRCDIR = /home/sander/Projects/webbridge/WPEFramework/Netflix
+WPEFRAMEWORK_PROVISIONING_OVERRIDE_SRCDIR = /home/sander/Projects/webbridge/WPEFramework/Provisioning
+WPEFRAMEWORK_REMOTECONTROL_OVERRIDE_SRCDIR = /home/sander/Projects/webbridge/WPEFramework/RemoteControl-Open
+WPEFRAMEWORK_SNAPSHOT_OVERRIDE_SRCDIR = /home/sander/Projects/webbridge/WPEFramework/Snapshot
+WPEFRAMEWORK_TRACECONTROL_OVERRIDE_SRCDIR = /home/sander/Projects/webbridge/WPEFramework/Tracing
+WPEFRAMEWORK_WEBKITBROWSER_OVERRIDE_SRCDIR = /home/sander/Projects/webbridge/WPEFramework/WebKitBrowser
+WPEFRAMEWORK_WEBPROXY_OVERRIDE_SRCDIR = /home/sander/Projects/webbridge/WPEFramework/WebProxy
+WPEFRAMEWORK_WEBSERVER_OVERRIDE_SRCDIR = /home/sander/Projects/webbridge/WPEFramework/WebServer
+WPEFRAMEWORK_OVERRIDE_SRCDIR = /home/sander/Projects/cppsdk/eclipse/src/cppsdk
+LIBPROVISION_OVERRIDE_SRCDIR = /home/sander/Projects/libprovision
+NETFLIX_OVERRIDE_SRCDIR = /home/sander/Projects/netflix/eclipse/src
+PLAYREADY_OVERRIDE_SRCDIR = /home/sander/Projects/playready
+
