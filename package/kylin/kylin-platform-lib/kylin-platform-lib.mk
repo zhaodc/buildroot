fb5c22aa620dceb1f30dd7852c3bbb64d10aa0e1
Bram Oosterhuis <bram.oosterhuis.nl@gmail.com>
2017-03-24 11:58:59 +0100

    [kylin-platform-lib] correct kernel modules install directory

--- package/kylin/kylin-platform-lib/kylin-platform-lib.mk
+++ package/kylin/kylin-platform-lib/kylin-platform-lib.mk
@@ -64,12 +64,12 @@ endif
 ifeq ($(BR2_PACKAGE_KYLIN_KERNEL_4_1_17)$(BR2_PACKAGE_KYLIN_PLATFORM_BIN_KO),yy)
 define KYLIN_PLATFORM_LIB_INSTALL_KERNEL_MODULES
   $(INSTALL) -d -m 0755 $(1)/usr/lib/modules
-  cp -av $(@D)/modules/4.1.17 $(1)/usr/lib/modules
+  cp -av $(@D)/modules/4.1.17 $(1)/lib/modules
 endef
 else ifeq ($(BR2_PACKAGE_KYLIN_KERNEL_4_1_35)$(BR2_PACKAGE_KYLIN_PLATFORM_BIN_KO),yy)
 define KYLIN_PLATFORM_LIB_INSTALL_KERNEL_MODULES
   $(INSTALL) -d -m 0755 $(1)/usr/lib/modules
-  cp -av $(@D)/modules/4.1.35 $(1)/usr/lib/modules
+  cp -av $(@D)/modules/4.1.35 $(1)/lib/modules
 endef
 else
   define KYLIN_PLATFORM_LIB_INSTALL_KERNEL_MODULES
