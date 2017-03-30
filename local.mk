afbdf6931d59ac9f10ca97c1248a5d0cc233c890
Bram Oosterhuis <bram.oosterhuis.nl@gmail.com>
2017-03-31 00:43:58 +0200

    [kylin] Integration of kernel and modules

---
+++ local.mk
@@ -0,0 +1,1 @@
+#KYLIN_DRIVERS_OVERRIDE_SRCDIR = /media/bram/build-ext/realtek/kylin-drivers
---
+++ package/kylin/kylin-drivers/kylin-drivers.mk
@@ -0,0 +1,157 @@
+################################################################################
+#
+# kylin-drivers
+#
+################################################################################
+
+KYLIN_DRIVERS_VERSION = 0.0.1
+KYLIN_DRIVERS_SITE_METHOD = git
+KYLIN_DRIVERS_SITE = git@github.com:Metrological/kylin-drivers.git
+KYLIN_DRIVERS_INSTALL_STAGING = YES
+KYLIN_DRIVERS_DEPENDENCIES += linux
+
+KYLIN_DRIVER_MAKE_ENV = TOP=$(@D) \
+                         CROSS=$(TARGET_CROSS) \
+                         CROSS_COMPILE=$(TARGET_CROSS) \
+                         KERNEL_SRC=${LINUX_DIR} \
+                         ARCH=arm64
+
+KYLIN_DRIVER_MALI_MAKE_ENV = $(KYLIN_DRIVER_MAKE_ENV) \
+                              MALI_ARCH=arm64 \
+                              MALI_CROSS_COMPILE=$(TARGET_CROSS) \
+                              TARGET_KDIR=${LINUX_DIR} \
+                              KERNEL_PATH=${LINUX_DIR} \
+                              KERNEL_VERSION=$(LINUX_VERSION_PROBED) \
+                              SOURCE_ROOT_DIR=$(@D)    
+                       
+KYLIN_RF4CE_DRIVER_MAKE_ENV = $(KYLIN_DRIVER_MAKE_ENV) \
+                              GP_CHIP=GP712
+
+KYLIN_PARAGON_DRIVER_MAKE_ENV = $(KYLIN_DRIVER_MAKE_ENV) \
+                                KERNEL_TARGET_CHIP=kylin \
+                                UFSD_HOST=$(patsubst %-,%,$(TARGET_CROSS))
+
+
+ifeq ($(BR2_PACKAGE_KYLIN_DRIVERS_MALI),y)
+define KYLIN_MALI_DRIVER_BUILD
+    $(MAKE) $(KYLIN_DRIVER_MALI_MAKE_ENV) $(TARGET_CONFIGURE_OPTS) -C $(@D)/drivers/gpu/mali build
+    $(MAKE) $(KYLIN_DRIVER_MALI_MAKE_ENV) $(TARGET_CONFIGURE_OPTS) -C $(@D)/drivers/gpu/mali strip
+endef
+define KYLIN_MALI_DRIVER_INSTALL
+    $(MAKE) $(KYLIN_DRIVER_MALI_MAKE_ENV) INSTALL_MOD_PATH=${1} $(TARGET_CONFIGURE_OPTS) -C $(@D)/drivers/gpu/mali install
+endef
+else
+define KYLIN_MALI_DRIVER_BUILD
+endef
+define KYLIN_MALI_DRIVER_INSTALL
+endef
+endif
+
+
+ifeq ($(BR2_PACKAGE_KYLIN_DRIVERS_WIFI),y)
+define KYLIN_WIFI_DRIVER_BUILD
+    $(MAKE) $(KYLIN_DRIVER_MAKE_ENV) $(TARGET_CONFIGURE_OPTS) -C $(@D)/drivers/wifi all
+endef
+define KYLIN_WIFI_DRIVER_INSTALL
+    $(MAKE) $(KYLIN_DRIVER_MAKE_ENV) INSTALL_MOD_PATH=${1} $(TARGET_CONFIGURE_OPTS) -C $(@D)/drivers/wifi modules_install
+endef
+else
+define KYLIN_WIFI_DRIVER_BUILD
+endef
+define KYLIN_WIFI_DRIVER_INSTALL
+endef
+endif
+
+
+ifeq ($(BR2_PACKAGE_KYLIN_DRIVERS_BLUETOOTH),y)
+define KYLIN_BT_DRIVER_BUILD
+    $(MAKE) $(KYLIN_DRIVER_MAKE_ENV) $(TARGET_CONFIGURE_OPTS) -C $(@D)/drivers/bluetooth/rtk_btusb all
+    $(MAKE) $(KYLIN_DRIVER_MAKE_ENV) $(TARGET_CONFIGURE_OPTS) -C $(@D)/drivers/bluetooth/rtk_btusb strip
+endef
+define KYLIN_BT_DRIVER_INSTALL
+    $(MAKE) $(KYLIN_DRIVER_MAKE_ENV) INSTALL_MOD_PATH=${1} $(TARGET_CONFIGURE_OPTS) -C $(@D)/drivers/bluetooth/rtk_btusb modules_install
+endef
+else
+define KYLIN_BT_DRIVER_BUILD
+endef
+define KYLIN_BT_DRIVER_INSTALL
+endef
+endif
+
+
+ifeq ($(BR2_PACKAGE_KYLIN_DRIVERS_RF4CE),y)
+define KYLIN_RF4CE_DRIVER_BUILD
+    $(MAKE) $(KYLIN_RF4CE_DRIVER_MAKE_ENV) $(TARGET_CONFIGURE_OPTS) -C $(@D)/drivers/rf4ce/gp712_driver all
+    $(MAKE) $(KYLIN_RF4CE_DRIVER_MAKE_ENV) $(TARGET_CONFIGURE_OPTS) -C $(@D)/drivers/rf4ce/gp712_driver strip
+endef
+define KYLIN_RF4CE_DRIVER_INSTALL
+    $(MAKE) $(KYLIN_RF4CE_DRIVER_MAKE_ENV) INSTALL_MOD_PATH=${1} $(TARGET_CONFIGURE_OPTS) -C $(@D)/drivers/rf4ce/gp712_driver modules_install
+endef
+else
+define KYLIN_RF4CE_DRIVER_BUILD
+endef
+define KYLIN_RF4CE_DRIVER_INSTALL
+endef
+endif
+
+
+ifeq ($(BR2_PACKAGE_KYLIN_DRIVERS_PARAGON),y)
+define KYLIN_PARAGON_DRIVER_BUILD
+    $(MAKE) $(KYLIN_PARAGON_DRIVER_MAKE_ENV) $(TARGET_CONFIGURE_OPTS) -C $(@D)/drivers/paragon all
+endef
+define KYLIN_PARAGON_DRIVER_INSTALL
+    $(MAKE) $(KYLIN_PARAGON_DRIVER_MAKE_ENV) INSTALL_MOD_PATH=${1} $(TARGET_CONFIGURE_OPTS) -C $(@D)/drivers/paragon modules_install
+endef
+else
+define KYLIN_PARAGON_DRIVER_BUILD
+endef
+define KYLIN_PARAGON_DRIVER_INSTALL
+endef
+endif
+
+ifeq ($(BR2_PACKAGE_KYLIN_DRIVERS_REALTEK),y)
+define KYLIN_REALTEK_DRIVERS_BUILD
+    $(MAKE) $(KYLIN_DRIVER_MAKE_ENV) $(TARGET_CONFIGURE_OPTS) -C $(@D)/osal all
+    $(MAKE) $(KYLIN_DRIVER_MAKE_ENV) $(TARGET_CONFIGURE_OPTS) -C $(@D)/platform_lib/ion all
+endef
+define KYLIN_REALTEK_DRIVERS_INSTALL
+  $(INSTALL) -m 0755 -d ${1}/usr/lib 
+  $(INSTALL) -m 0755 $(@D)/osal/*.so ${1}/usr/lib
+endef
+else
+define KYLIN_REALTEK_DRIVERS_BUILD
+endef
+define KYLIN_REALTEK_DRIVERS_INSTALL
+endef
+endif
+
+define KYLIN_DRIVERS_BUILD_CMDS
+    $(call KYLIN_MALI_DRIVER_BUILD)
+    $(call KYLIN_WIFI_DRIVER_BUILD)
+    $(call KYLIN_BT_DRIVER_BUILD)
+    $(call KYLIN_RF4CE_DRIVER_BUILD)
+    $(call KYLIN_PARAGON_DRIVER_BUILD)
+    $(call KYLIN_REALTEK_DRIVERS_BUILD)
+endef
+
+define KYLIN_DRIVERS_INSTALL_STAGING_CMDS
+    $(call KYLIN_MALI_DRIVER_INSTALL,${STAGING_DIR})
+    $(call KYLIN_WIFI_DRIVER_INSTALL,${STAGING_DIR})
+    $(call KYLIN_BT_DRIVER_INSTALL,${STAGING_DIR})
+    $(call KYLIN_RF4CE_DRIVER_INSTALL,${STAGING_DIR})
+    $(call KYLIN_PARAGON_DRIVER_INSTALL,${STAGING_DIR})
+    $(call KYLIN_REALTEK_DRIVERS_INSTALL,${STAGING_DIR})
+endef
+
+define KYLIN_DRIVERS_INSTALL_TARGET_CMDS
+    $(call KYLIN_MALI_DRIVER_INSTALL,${TARGET_DIR})
+    $(call KYLIN_WIFI_DRIVER_INSTALL,${TARGET_DIR})
+    $(call KYLIN_BT_DRIVER_INSTALL,${TARGET_DIR})
+    $(call KYLIN_RF4CE_DRIVER_INSTALL,${TARGET_DIR})
+    $(call KYLIN_PARAGON_DRIVER_INSTALL,${TARGET_DIR})
+    $(call KYLIN_REALTEK_DRIVERS_INSTALL,${TARGET_DIR})
+endef
+
+$(eval $(generic-package))
+
+
