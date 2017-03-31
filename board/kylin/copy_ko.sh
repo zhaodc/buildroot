0c6f55fbe1eb710b0a63bb280dceb01283d9e9ac
Bram Oosterhuis <bram.oosterhuis.nl@gmail.com>
2017-03-31 10:03:25 +0200

    [kylin kernel] only export kernel and kernel modules

---
+++ board/kylin/copy_ko.sh
@@ -0,0 +1,21 @@
+#!/bin/sh
+BOARD_DIR="$(dirname $0)"
+ROOTFS_DIR="${BINARIES_DIR}/../rootfs"
+ROOTFS_FILES="${BINARIES_DIR}/rootfs.files"
+
+echo "Binaries: ${BINARIES_DIR}"
+echo "Target: ${TARGET_DIR}"
+
+# Temp rootfs dir
+mkdir -p "${ROOTFS_DIR}"
+
+echo "/lib/modules" >> "${ROOTFS_FILES}"
+
+rsync -ar --files-from="${ROOTFS_FILES}" "${TARGET_DIR}" "${ROOTFS_DIR}"
+
+# Create tar
+tar -cvf "${BINARIES_DIR}/kernel-modules.tar" -C "${ROOTFS_DIR}" .
+
+# Cleaning up
+rm -rf "${ROOTFS_FILES}"
+rm -rf "${ROOTFS_DIR}"
