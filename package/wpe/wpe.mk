6939916bdfbb2e251c8fc25aee90767f8d87b964
Adrian Perez de Castro <aperez@igalia.com>
2017-04-04 05:21:16 +0300

    Remove getMediaStreamDevices patch, as it's now in the WPE branch we build

--- package/wpe/wpe.mk
+++ package/wpe/wpe.mk
@@ -4,8 +4,10 @@
 #
 ################################################################################
 
-WPE_VERSION = 779249adf98b1efa99ea81c4be4bda9101d1d76b
-WPE_SITE = $(call github,Metrological,WebKitForWayland,$(WPE_VERSION))
+# Tip of the aperezdc/webrtc branch from
+# https://github.com/WebPlatformForEmbedded/WPEWebKit/tree/aperez/webrtc
+WPE_VERSION = acc62511276236dc975f84197534a54df8088bb9
+WPE_SITE = $(call github,WebPlatformForEmbedded,WPEWebKit,$(WPE_VERSION))
 
 WPE_INSTALL_STAGING = YES
 
