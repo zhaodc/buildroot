e7ea45dc69da280d415b8d9317dbc9fa3cafecd6
Bram Oosterhuis <bram.oosterhuis.nl@gmail.com>
2017-04-11 18:47:43 +0200

    Add metrological ip page as default

--- board/kylin/copy_configs.sh
+++ board/kylin/copy_configs.sh
@@ -7,3 +7,9 @@ cp -v $BOARD_DIR/config/wpa_ap.conf $TARGET_DIR/etc/ap_wlan0
 cp -v $BOARD_DIR/config/wlan_dhcp_ap.conf $TARGET_DIR/etc/ap_wlan0
 cp -v $BOARD_DIR/config/interfaces $TARGET_DIR/etc/network
 cp -v $BOARD_DIR/config/S41startAP $TARGET_DIR/etc/init.d
+
+# Copy index.html page for WPE Framework
+if [ -f "${BOARD_DIR}/index.html" ]; then
+	mkdir -p "${TARGET_DIR}/www/"
+	cp -pvf "${BOARD_DIR}/index.html" "${TARGET_DIR}/www/"
+fi
