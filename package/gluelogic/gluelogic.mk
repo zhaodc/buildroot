ff4b1b8046d47215c525d641dba582b11d88a574
Wouter Meek <woutermeek@gmail.com>
2017-04-16 03:38:44 -0700

    [gluelogic] bump to latest

--- package/gluelogic/gluelogic.mk
+++ package/gluelogic/gluelogic.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-GLUELOGIC_VERSION = 526776d49d9232a40834775e6e9ee74e351e86ad
+GLUELOGIC_VERSION = d83eabfde9fdea3fff85a266417abbeba4ca30cb
 GLUELOGIC_SITE_METHOD = git
 GLUELOGIC_SITE = git@github.com:Metrological/gluelogic.git
 GLUELOGIC_INSTALL_STAGING = YES
