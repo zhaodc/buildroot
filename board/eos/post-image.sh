1382016ddc5787b2d7b62d667d4301a3162e41ee
Bram Oosterhuis <bram.oosterhuis.nl@gmail.com>
2017-04-26 14:58:23 +0200

    [EOS] Add packaging tool

--- board/eos/post-image.sh
+++ board/eos/post-image.sh
@@ -1,3 +1,8 @@
 #!/bin/sh
+set -x
+set -e
+
 BOARD_DIR="$(dirname $0)"
-#${BOARD_DIR}/eos-package-utility/arris_package_oe.sh  ${BINARIES_DIR} rootfs.cpio.xz
+
+[ -d ${HOST_DIR}/arris-packaging-tool ] ||  git clone git@github.com:Metrological/arris-packaging-tool.git ${HOST_DIR}/arris-packaging-tool
+[ -f ${HOST_DIR}/arris-packaging-tool/arris_package_br.sh ] && ${HOST_DIR}/arris-packaging-tool/arris_package_br.sh  ${BINARIES_DIR} rootfs.cpio.xz
