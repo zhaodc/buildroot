2fe3cd1b79b15ab4b7a73cf364b99e595dccce48
Rene Barto <rene.barto@vodafonethuis.nl>
2017-06-29 18:39:15 +0200

    Split weston-apps, created separate package kylin-apps for kylin-specific code.

---
+++ package/kylin/kylin-apps/kylin-apps.mk
@@ -0,0 +1,44 @@
+################################################################################
+#
+# kylin-apps
+#
+################################################################################
+
+KYLIN_APPS_VERSION = 0.0.0
+KYLIN_APPS_SITE = 
+KYLIN_APPS_SOURCE = 
+KYLIN_APPS_INSTALL_STAGING = YES
+KYLIN_APPS_INSTALL_TARGET = YES
+
+KYLIN_APPS_CONF_OPTS += -DPLATFORM_LIB_INCLUDE=$(STAGING_DIR)/usr/include/realtek
+KYLIN_APPS_CONF_OPTS += -DPLATFORM_LIB_LIBDIR=$(STAGING_DIR)/usr/lib
+
+ifeq ($(BR2_PACKAGE_KYLIN_HDMI_INFO),y)
+KYLIN_APPS_DEPENDENCIES += udev kylin-platform-lib kylin-graphics
+KYLIN_APPS_CONF_OPTS += -DENABLE_KYLIN_HDMI_INFO=ON
+else
+KYLIN_APPS_CONF_OPTS += -DENABLE_KYLIN_HDMI_INFO=OFF
+endif
+
+ifeq ($(BR2_PACKAGE_KYLIN_HDMI_MODESET),y)
+KYLIN_APPS_DEPENDENCIES += udev kylin-platform-lib kylin-graphics
+KYLIN_APPS_CONF_OPTS += -DENABLE_KYLIN_HDMI_MODESET=ON
+else
+KYLIN_APPS_CONF_OPTS += -DENABLE_KYLIN_HDMI_MODESET=OFF
+endif
+
+ifeq ($(BR2_PACKAGE_KYLIN_SIMPLE_APP),y)
+KYLIN_APPS_DEPENDENCIES += udev kylin-platform-lib kylin-graphics
+KYLIN_APPS_CONF_OPTS += -DENABLE_KYLIN_SIMPLE_APP=ON
+else
+KYLIN_APPS_CONF_OPTS += -DENABLE_KYLIN_SIMPLE_APP=OFF
+endif
+
+ifeq ($(BR2_PACKAGE_KYLIN_VIDEO_INFO),y)
+KYLIN_APPS_DEPENDENCIES += udev kylin-platform-lib kylin-graphics
+KYLIN_APPS_CONF_OPTS += -DENABLE_KYLIN_VIDEO_INFO=ON
+else
+KYLIN_APPS_CONF_OPTS += -DENABLE_KYLIN_VIDEO_INFO=OFF
+endif
+
+$(eval $(cmake-package))
