4e7d3f6ee21914f0e03c542f685e2131b869baea
modeveci <o.deveci@metrological.com>
2017-12-18 23:49:32 +0100

    [UMA][wpebackend-rdk]: bump the version

--- package/wpe/wpebackend-rdk/wpebackend-rdk.mk
+++ package/wpe/wpebackend-rdk/wpebackend-rdk.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-WPEBACKEND_RDK_VERSION = bff90468cbda5276357ccb34a3674b62c65e0150
+WPEBACKEND_RDK_VERSION = 1abc9e3168844b7f9e3b594f6e6056b5917f16a5
 WPEBACKEND_RDK_SITE = $(call github,WebPlatformForEmbedded,WPEBackend-rdk,$(WPEBACKEND_RDK_VERSION))
 WPEBACKEND_RDK_INSTALL_STAGING = YES
 WPEBACKEND_RDK_DEPENDENCIES = wpebackend libglib2
