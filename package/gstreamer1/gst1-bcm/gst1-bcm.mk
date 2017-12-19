d2af3b1915746d7006aa2a8fd8b7cbb240c75c1d
modeveci <o.deveci@metrological.com>
2017-12-19 13:23:45 +0100

    [UMA]: bump the gst1-bcm version for refsw 16.1

--- package/gstreamer1/gst1-bcm/gst1-bcm.mk
+++ package/gstreamer1/gst1-bcm/gst1-bcm.mk
@@ -5,7 +5,7 @@
 ################################################################################
 
 ifeq ($(BR2_PACKAGE_BCM_REFSW_16_1),y)
-GST1_BCM_VERSION = 17.1-3
+GST1_BCM_VERSION = 17.1-4
 else ifeq ($(BR2_PACKAGE_BCM_REFSW_16_2),y)
 GST1_BCM_VERSION = 16.2
 else ifeq ($(BR2_PACKAGE_BCM_REFSW_17_1_RDK),y)
