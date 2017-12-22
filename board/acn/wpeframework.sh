c08f039aedafc3ba43d03812ccee1b24477377b3
modeveci <o.deveci@metrological.com>
2017-12-22 11:00:50 +0100

    [ACN]: After closure of phase1, ACN shared a new FW which launchs WPEFramework from their UI by pressing Youtube button on On Demand Menu. To do that, our development binaries should take place accordantly. Now we have 'acn_bind', 'metrological' and empty options for wpeframework.sh 1. For integrated solution, we have to launch 'acn_bind' to override ACN paths for our binaries. This launch should be performed right after bootup And before wpeframework launch by ACN. 2. For standalone launch, we can still use 'metrological' option to behave as earlier without ACN interactions 3. after 'acn_bind', ACN middleware will launch wpeframework.sh without parameters to launch WPEFramework
    Further questions, ping O.Deveci :-)

--- board/acn/wpeframework.sh
+++ board/acn/wpeframework.sh
@@ -1,40 +1,65 @@
 #!/bin/sh
-export SOURCE=/hdd/metrological
-export ACN=/hdd/acn
-export LD_LIBRARY_PATH=$SOURCE/usr/lib:/lib:/usr/lib:$SOURCE/lib
-export PATH=$PATH:$SOURCE/usr/bin
 
+export SOURCE=/hdd/metrological
+export LD_LIBRARY_PATH=$SOURCE/usr/lib:/lib:/usr/lib:$SOURCE/lib
+export PATH=$SOURCE/usr/bin:$PATH
 export GST_PLUGIN_SCANNER=$SOURCE/usr/libexec/gstreamer-1.0/gst-plugin-scanner
 export GST_PLUGIN_SYSTEM_PATH=$SOURCE/usr/lib/gstreamer-1.0
 
-# Currently the root system is read-only. Since we cannot add anything there we bind 
-# existing directories with a copy of the actual system. All the stuff we want to 
-# add is symbolicly linked in from our sources..
-if [ ! -d $ACN ]; then
+case "$1" in
+bind_acn)
+	grep -q "/opt/wpe ext4" /proc/mounts &&
+		echo "/opt/wpe is already mounted" || mount -t ext4 --bind $SOURCE /opt/wpe
+	grep -q "/etc/ssl ext4" /proc/mounts &&
+		echo "/etc/ssl is already mounted" || mount -t ext4 --bind $SOURCE/etc/ssl/ /etc/ssl/
+	grep -q "/usr/lib/gio ext4" /proc/mounts &&
+		echo "/usr/lib/gio is already mounted" || mount -t ext4 --bind $SOURCE/usr/lib/gio /usr/lib/gio
+;;
+metrological)
+	export DESTINATION=/hdd/acn
+	
+	# Currently the root system is read-only. Since we cannot add anything there we bind 
+	# existing directories with a copy of the actual system. All the stuff we want to 
+	# add is symbolicly linked in from our sources..
+	if [ ! -d $DESTINATION ]; then
 
-mkdir -p $ACN/share
-mkdir -p $ACN/etc
-mkdir -p $ACN/lib
-cp -rfap /usr/share/* $ACN/share
-cp -rfap /etc/* $ACN/etc
-cp -rfap /usr/lib/* $ACN/lib
+		mkdir -p $DESTINATION/share
+		mkdir -p $DESTINATION/etc
+		mkdir -p $DESTINATION/lib
+		cp -rfap /usr/share/* $DESTINATION/share
+		cp -rfap /etc/* $DESTINATION/etc
+		cp -rfap /usr/lib/* $DESTINATION/lib
 
-ln -s $SOURCE/usr/share/mime $ACN/share/mime
-ln -s $SOURCE/usr/share/X11 $ACN/share/X11
-ln -s $SOURCE/usr/share/WPEFramework $ACN/share/WPEFramework
-ln -s $SOURCE/usr/share/fonts $ACN/share/fonts
-ln -s $SOURCE/etc/ssl $ACN/etc/ssl
-ln -s $SOURCE/etc/ssl $ACN/lib/ssl
-ln -s $SOURCE/etc/fonts $ACN/etc/fonts
-ln -s $SOURCE/etc/WPEFramework $ACN/etc/WPEFramework
-ln -s $SOURCE/usr/lib/gio $ACN/lib/gio
-fi
+		ln -s $SOURCE/usr/share/mime $DESTINATION/share/mime
+		ln -s $SOURCE/usr/share/X11 $DESTINATION/share/X11
+		ln -s $SOURCE/usr/share/WPEFramework $DESTINATION/share/WPEFramework
+		ln -s $SOURCE/usr/share/fonts $DESTINATION/share/fonts
+		ln -s $SOURCE/etc/ssl $DESTINATION/etc/ssl
+		ln -s $SOURCE/etc/ssl $DESTINATION/lib/ssl
+		ln -s $SOURCE/etc/fonts $DESTINATION/etc/fonts
+		ln -s $SOURCE/etc/WPEFramework $DESTINATION/etc/WPEFramework
+		ln -s $SOURCE/usr/lib/gio $DESTINATION/lib/gio
+	fi
+	grep -q "/usr/share ext4" /proc/mounts && echo "/usr/share is already mounted" || mount -t ext4 --bind $DESTINATION/share/ /usr/share/
+	grep -q "/etc ext4" /proc/mounts && echo "/etc is already mounted" || mount -t ext4 --bind $DESTINATION/etc/ /etc/
+	grep -q "/usr/lib ext4" /proc/mounts && echo "/usr/lib is already mounted" || mount -t ext4 --bind $DESTINATION/lib/ /usr/lib/
 
-grep -q "/usr/share ext4" /proc/mounts && echo "/usr/share is already mounted" || mount -t ext4 --bind $ACN/share/ /usr/share/
-grep -q "/etc ext4" /proc/mounts && echo "/etc is already mounted" || mount -t ext4 --bind $ACN/etc/ /etc/
-grep -q "/usr/lib ext4" /proc/mounts && echo "/usr/lib is already mounted" || mount -t ext4 --bind $ACN/lib/ /usr/lib/
+	#work around for playready
+	cd /usr/bin/netflix
 
-#work around for playready
-cd /usr/bin/netflix
+	LD_PRELOAD=$SOURCE/lib/libstdc\+\+.so.6.0.21 WPEFramework
+;;
 
-LD_PRELOAD=$SOURCE/lib/libstdc\+\+.so.6.0.21 WPEFramework
+*)
+	grep -q "/opt/wpe ext4" /proc/mounts && 
+		echo "/opt/wpe is already mounted" || mount -t ext4 --bind /hdd/metrological /opt/wpe
+	grep -q "/etc/ssl ext4" /proc/mounts && 
+		echo "/etc/ssl is already mounted" || mount -t ext4 --bind /hdd/metrological/etc/ssl/ /etc/ssl/
+	grep -q "/usr/lib/gio ext4" /proc/mounts &&
+		echo "/usr/lib/gio is already mounted" || mount -t ext4 --bind $SOURCE/usr/lib/gio /usr/lib/gio
+	
+	cd /usr/bin/netflix	
+	LD_PRELOAD=$SOURCE/lib/libstdc\+\+.so.6.0.21 WPEFramework -c $SOURCE/etc/WPEFramework/config.json 	
+;;
+esac
+
