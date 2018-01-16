36ab95a4c6032ebb74b341d8e81e8d1fb30521ae
Sander van der Maar <sandervandermaar@gmail.com>
2018-01-16 11:17:23 +0100

    Selects harfbuzz 1.2.5

--- package/harfbuzz/harfbuzz.mk
+++ package/harfbuzz/harfbuzz.mk
@@ -4,10 +4,10 @@
 #
 ################################################################################
 
-HARFBUZZ_VERSION = 1.4.4
-ifeq ($(BR2_PACKAGE_NETFLIX),y)
-HARFBUZZ_VERSION = 1.0.1
-endif
+HARFBUZZ_VERSION = 1.2.5
+#ifeq ($(BR2_PACKAGE_NETFLIX),y)
+#HARFBUZZ_VERSION = 1.0.1
+#endif
 HARFBUZZ_SITE = https://www.freedesktop.org/software/harfbuzz/release
 HARFBUZZ_SOURCE = harfbuzz-$(HARFBUZZ_VERSION).tar.bz2
 HARFBUZZ_LICENSE = MIT, ISC (ucdn library)
