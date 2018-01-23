e79bdebdb5aaa49a41ba39c7fcb2ce311b8c380d
modeveci <o.deveci@metrological.com>
2018-01-23 21:21:48 +0100

    [ACN]: OpenCDM integration

--- board/acn/wpeframework.sh
+++ board/acn/wpeframework.sh
@@ -1,7 +1,8 @@
 #!/bin/sh
 
 export SOURCE=/hdd/metrological
-export LD_LIBRARY_PATH=$SOURCE/usr/lib:/lib:/usr/lib:$SOURCE/lib
+#export LD_LIBRARY_PATH=$SOURCE/usr/lib:/lib:/usr/lib:$SOURCE/lib
+export LD_LIBRARY_PATH=$SOURCE/usr/lib:/lib:/usr/lib:$SOURCE/lib:$SOURCE/usr/lib/wpeframework/plugins:$SOURCE/usr/lib/wpeframework/proxystubs
 export PATH=$SOURCE/usr/bin:$PATH
 export GST_PLUGIN_SCANNER=$SOURCE/usr/libexec/gstreamer-1.0/gst-plugin-scanner
 export GST_PLUGIN_SYSTEM_PATH=$SOURCE/usr/lib/gstreamer-1.0
@@ -26,9 +27,11 @@ metrological)
 		mkdir -p $DESTINATION/share
 		mkdir -p $DESTINATION/etc
 		mkdir -p $DESTINATION/lib
+		mkdir -p $DESTINATION/bin
 		cp -rfap /usr/share/* $DESTINATION/share
 		cp -rfap /etc/* $DESTINATION/etc
 		cp -rfap /usr/lib/* $DESTINATION/lib
+		cp -rfap /usr/bin/* $DESTINATION/bin
 
 		ln -s $SOURCE/usr/share/mime $DESTINATION/share/mime
 		ln -s $SOURCE/usr/share/X11 $DESTINATION/share/X11
@@ -36,6 +39,7 @@ metrological)
 		ln -s $SOURCE/usr/share/fonts $DESTINATION/share/fonts
 		ln -s $SOURCE/etc/ssl $DESTINATION/etc/ssl
 		ln -s $SOURCE/etc/ssl $DESTINATION/lib/ssl
+		ln -s $SOURCE/etc/netconfig $DESTINATION/etc/netconfig
 		ln -s $SOURCE/etc/fonts $DESTINATION/etc/fonts
 		ln -s $SOURCE/etc/WPEFramework $DESTINATION/etc/WPEFramework
 		ln -s $SOURCE/usr/lib/gio $DESTINATION/lib/gio
@@ -43,7 +47,17 @@ metrological)
 	grep -q "/usr/share ext4" /proc/mounts && echo "/usr/share is already mounted" || mount -t ext4 --bind $DESTINATION/share/ /usr/share/
 	grep -q "/etc ext4" /proc/mounts && echo "/etc is already mounted" || mount -t ext4 --bind $DESTINATION/etc/ /etc/
 	grep -q "/usr/lib ext4" /proc/mounts && echo "/usr/lib is already mounted" || mount -t ext4 --bind $DESTINATION/lib/ /usr/lib/
+	grep -q "/usr/bin ext4" /proc/mounts && echo "/usr/bin is already mounted" || mount -t ext4 --bind $DESTINATION/bin /usr/bin/
 
+	#launch rpcbind to create channel between framework and webkit
+	ext=`pidof rpcbind`
+	if [ ! $ext ]; then
+    		echo "Launching rpcbind ..."
+		rpcbind 
+	else
+    		echo "rpcbind is already launched..."
+	fi	
+	
 	#work around for playready
 	cd /usr/bin/netflix
 
@@ -58,6 +72,15 @@ metrological)
 	grep -q "/usr/lib/gio ext4" /proc/mounts &&
 		echo "/usr/lib/gio is already mounted" || mount -t ext4 --bind $SOURCE/usr/lib/gio /usr/lib/gio
 	
+	#launch rpcbind to create channel between framework and webkit
+	ext=`pidof rpcbind`
+	if [ ! $ext ]; then
+    		echo "Launching rpcbind ..."
+		rpcbind 
+	else
+    		echo "rpcbind is already launched..."
+	fi	
+
 	cd /usr/bin/netflix	
 	LD_PRELOAD=$SOURCE/lib/libstdc\+\+.so.6.0.21 WPEFramework -c $SOURCE/etc/WPEFramework/config.json 	
 ;;
